96ce4ab5d6bd17f04f6023287c2945dd96df4cfd
Max Heidrich <heidrich-max@example.com>
2026-02-27 10:27:50 +0100

    Enhance ls.php

--- public/ls.php
+++ public/ls.php
@@ -1,5 +1,13 @@
-<?php
+echo "<h3>Current Directory: " . __DIR__ . "</h3>";
 $files = scandir(__DIR__);
 echo "<pre>";
 print_r($files);
 echo "</pre>";
+
+if (is_dir(__DIR__ . '/public')) {
+    echo "<h3>Public Directory:</h3>";
+    $filesPublic = scandir(__DIR__ . '/public');
+    echo "<pre>";
+    print_r($filesPublic);
+    echo "</pre>";
+}
